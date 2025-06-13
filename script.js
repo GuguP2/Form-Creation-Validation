345a4c761fe2730f1f0791e892c73794e8a4f14f
Gugu Mthethwa <pociamthethwa@gmail.com>
2025-06-13 16:26:44 +0200

    newonw

--- script.js
+++ script.js
@@ -19,6 +19,20 @@ if (!email.includes('@') || !email.includes('.')) {
   isValid = false;
   messages.push("Email must include both '@' and '.' characters.");
 }
-
+if (password.length < 8) {
+  isValid = false;
+  messages.push("Password must be at least 8 characters long.");
 }
+if(feedbackDiv){
+isValid = true;
+messages.push("Registration successful!");
+feedbackDiv.style.color = #28a745;
+} else {
+  if (feedbackDiv){
+    isValid = false;
+    messages.push(messages + br);
+    feedbackDiv.style.color = #dc3545;
+  }
+}
+
 }
