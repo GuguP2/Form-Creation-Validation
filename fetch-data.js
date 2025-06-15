6a9dbe92df4f1a713a47d57cda7ece5b1f31515c
Gugu Mthethwa <pociamthethwa@gmail.com>
2025-06-15 09:00:29 +0200

    Update fetch-data.js

--- fetch-data.js
+++ fetch-data.js
@@ -1,11 +1,3 @@
 function fetchUserData(){
   const apiUrl = 'https://jsonplaceholder.typicode.com/users';
-  const dataContainer = document.getElementById('api-data');
-
-  try {
-    const response = await fetch(apiUrl);
-    const users = await response.json();
-    dataContainer.innerHTML = '';
-  }
-  const userList = document.getElementById('user-list');
-} 
+}
